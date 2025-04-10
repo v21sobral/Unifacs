82bad4152a68f472c404efd0d1373d28a3764065
21 Larbos <49079321+v21sobral@users.noreply.github.com>
2025-04-10 05:10:53 +0000

    saveme

--- lab04/index.js
+++ lab04/index.js
@@ -6,3 +6,16 @@ app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000.');
     console.log('Acesse a url http://localhost:3000');
 });
+
+app.get('/', (req, res) => res.send('Servidor de boas!'));
+
+app.get('/jogos', (req, res) => {
+    let jogos = [
+        {nome: "Super Mario World", ano: 1990, categoria: "Plataforma"},
+        {nome: "Age of Empires II", ano: 1999, categoria: "Estratégia"}
+    
+
+    ];
+
+    res.send(jogos);
+});
