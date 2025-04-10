324621df0f72def1be4bbd19654d67c5b80de75e
21 Larbos <49079321+v21sobral@users.noreply.github.com>
2025-04-10 04:43:48 +0000

    salvo

---
+++ lab04/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000.');
+    console.log('Acesse a url http://localhost:3000');
+});
